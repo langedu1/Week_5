f10a35db90e57e6446c5bf2d764b86ebdbf15901
Eduard <langedu1@students.zhaw.ch>
2024-05-10 13:29:52 +0200

    ID 4

---
+++ demo/src/test/java/ch/langedu1/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.langedu1.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
